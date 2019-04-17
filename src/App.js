86c9d7b1dd04d9ccc209ed293c6d8b4b7487027f
Fernando <fernandowashimi@gmail.com>
2019-04-16 22:25:55 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    inputValue: "",
+    displayResult: false
+  };
+
+  handleInputChange = e => {
+    const char = e.target.value.slice(-1);
+    if (char === "") {
+      this.setState({ inputValue: "" });
+    } else if (char !== "0" && char !== "1") {
+      return;
+    } else {
+      this.setState({ inputValue: e.target.value });
+    }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ displayResult: true });
+    if (this.state.inputValue === "") {
+      this.setState({ result: "Valor digitado inválido" });
+    } else {
+      const array = this.state.inputValue.split("");
+      this.convertBinToDec(array);
+    }
+  };
+
+  convertBinToDec(value) {
+    const result = value.reverse().reduce((previous, current, index) => {
+      return previous + current * Math.pow(2, index);
+    }, 0);
+    this.setState({ result });
+    this.setState({ displayResult: true });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main-container">
+        <h1>Bin2Dec</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={this.state.inputValue}
+            onChange={this.handleInputChange}
+          />
+          <button type="submit">Converter</button>
+        </form>
+        {this.state.displayResult && (
+          <div className="result-container">
+            <h2>{this.state.result}</h2>
+          </div>
+        )}
       </div>
     );
   }
